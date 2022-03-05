d0ed44655033bd910cf864c8ea208295ef5b4bf8
ShiftTGC <ShiftTGC@gmail.com>
2022-03-05 19:57:27 +0100

    Add project files.

---
+++ SimpleMediaPlayerButtons/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace SimpleMediaPlayerButtons
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TrackPrev = new System.Windows.Forms.Button();
+            this.TrackPlayPause = new System.Windows.Forms.Button();
+            this.TrackNext = new System.Windows.Forms.Button();
+            this.TrackStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TrackPrev
+            // 
+            this.TrackPrev.Location = new System.Drawing.Point(12, 12);
+            this.TrackPrev.Name = "TrackPrev";
+            this.TrackPrev.Size = new System.Drawing.Size(116, 79);
+            this.TrackPrev.TabIndex = 0;
+            this.TrackPrev.Text = "TrackPrev";
+            this.TrackPrev.UseVisualStyleBackColor = true;
+            this.TrackPrev.Click += new System.EventHandler(this.TrackPrev_Click);
+            // 
+            // TrackPlayPause
+            // 
+            this.TrackPlayPause.Location = new System.Drawing.Point(134, 12);
+            this.TrackPlayPause.Name = "TrackPlayPause";
+            this.TrackPlayPause.Size = new System.Drawing.Size(116, 79);
+            this.TrackPlayPause.TabIndex = 1;
+            this.TrackPlayPause.Text = "TrackPlayPause";
+            this.TrackPlayPause.UseVisualStyleBackColor = true;
+            this.TrackPlayPause.Click += new System.EventHandler(this.TrackPlayPause_Click);
+            // 
+            // TrackNext
+            // 
+            this.TrackNext.Location = new System.Drawing.Point(256, 12);
+            this.TrackNext.Name = "TrackNext";
+            this.TrackNext.Size = new System.Drawing.Size(116, 79);
+            this.TrackNext.TabIndex = 2;
+            this.TrackNext.Text = "TrackNext";
+            this.TrackNext.UseVisualStyleBackColor = true;
+            this.TrackNext.Click += new System.EventHandler(this.TrackNext_Click);
+            // 
+            // TrackStop
+            // 
+            this.TrackStop.Location = new System.Drawing.Point(134, 97);
+            this.TrackStop.Name = "TrackStop";
+            this.TrackStop.Size = new System.Drawing.Size(116, 79);
+            this.TrackStop.TabIndex = 3;
+            this.TrackStop.Text = "TrackStop";
+            this.TrackStop.UseVisualStyleBackColor = true;
+            this.TrackStop.Click += new System.EventHandler(this.TrackStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(388, 193);
+            this.Controls.Add(this.TrackStop);
+            this.Controls.Add(this.TrackNext);
+            this.Controls.Add(this.TrackPlayPause);
+            this.Controls.Add(this.TrackPrev);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TrackPrev;
+        private System.Windows.Forms.Button TrackPlayPause;
+        private System.Windows.Forms.Button TrackNext;
+        private System.Windows.Forms.Button TrackStop;
+    }
+}
+
---
+++ SimpleMediaPlayerButtons/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+//Almost copy-paste pretty much... Was hoping there would be more of a learning explanation to it. 
+//Or that they would kind of explain the "why"s per say. They did not.
+//https://ourcodeworld.com/articles/read/128/how-to-play-pause-music-or-go-to-next-and-previous-track-from-windows-using-c-valid-for-all-windows-music-players
+//They did show me it tho and after a little bit of digging, well, I somewhat understand how it works now, but, eh, idk.
+//Code has been "made". If anything, I managed to add the "stop" button that basically doesn't get used anymore.
+//Small things, small things. 
+namespace SimpleMediaPlayerButtons
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public const int KEYEVENTF_EXTENTEDKEY = 1;
+        public const int KEYEVENTF_KEYUP = 0;
+        public const int VK_MEDIA_NEXT_TRACK = 0xB0;// code to jump to next track
+        public const int VK_MEDIA_PLAY_PAUSE = 0xB3;// code to play or pause a song
+        public const int VK_MEDIA_PREV_TRACK = 0xB1;// code to jump to prev track
+        public const int VK_MEDIA_STOP = 0xB2;// code to stop a song
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte virtualKey, byte scanCode, uint flags, IntPtr extraInfo);
+        private void TrackPlayPause_Click(object sender, EventArgs e)
+        {
+            keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+        }
+
+        private void TrackPrev_Click(object sender, EventArgs e)
+        {
+            keybd_event(VK_MEDIA_PREV_TRACK, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+        }
+
+        private void TrackNext_Click(object sender, EventArgs e)
+        {
+            keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+        }
+
+        private void TrackStop_Click(object sender, EventArgs e)
+        {
+            keybd_event(VK_MEDIA_STOP, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
+        }
+    }
+}
